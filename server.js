7b72dd0bfdedd46874e2c7f3f866b2f088feaf07
Aaron Wood <aaronjwood@gmail.com>
2012-11-14 20:30:13 -0500

    Added additional error handling for clients that might not correctly send back the appropriate data

--- server.js
+++ server.js
@@ -48,63 +48,68 @@ socket.sockets.on('connection', function(client) {
 	
 	//When a tracker emits a beacon then do necessary processing
 	client.on('beacon', function(data) {
-		payload.totalConnections++;
-		client["url"] = data.url;
-		
-		//The client id uniquely identifies a user
-		var userData = {
-				"sessionId": client.id,
-				"browser": Util.getBrowser(data.browser),
-				"screenWidth": data.screenWidth,
-				"screenHeight": data.screenHeight,
-				"os": Util.getOs(data.os)
-		};
-		
-		//Increment the appropriate browser count
-		payload.browsers.count[userData.browser]++;
-		var newUser = new User(userData);
-		
-		//If an object tracking the URL already exists then increment the number of connections and assign the new user
-		if(allTrackers[data.url]) {
-			allTrackers[data.url].numConnections++;
-			allTrackers[data.url].clients[client.id] = newUser;
+		try {
+			payload.totalConnections++;
+			client["url"] = data.url;
+			
+			//The client id uniquely identifies a user
+			var userData = {
+					"sessionId": client.id,
+					"browser": Util.getBrowser(data.browser),
+					"screenWidth": data.screenWidth,
+					"screenHeight": data.screenHeight,
+					"os": Util.getOs(data.os)
+			};
+			
+			//Increment the appropriate browser count
+			payload.browsers.count[userData.browser]++;
+			var newUser = new User(userData);
+			
+			//If an object tracking the URL already exists then increment the number of connections and assign the new user
+			if(allTrackers[data.url]) {
+				allTrackers[data.url].numConnections++;
+				allTrackers[data.url].clients[client.id] = newUser;
+			}
+			
+			//Otherwise create a new tracker and user and assign it to the URL
+			else {
+				var newTracker = new Tracker(newUser, data.url);
+				allTrackers[data.url] = newTracker;
+				allTrackers[data.url].numConnections = 1;
+			}
+			
+			//Get the string value for the screen resolution and add it to the payload if it doesn't exist
+			var screenResolution = newUser.getScreenResolution();
+			if(payload.screenResolutions[screenResolution]) {
+				payload.screenResolutions[screenResolution]++;
+			}
+			else {
+				payload.screenResolutions[screenResolution] = 1;
+			}
+			
+			//Add the OS to the payload if it doesn't exist
+			if(payload.os[userData.os]) {
+				payload.os[userData.os]++;
+			}
+			else {
+				payload.os[userData.os] = 1;
+			}
+			
+			//Send the data back
+			Tracker.sendPayload(allTrackers, payload, config, socket);
+		}
+		catch(error) {
+			console.log("\n\nA connection error has occured:\n" + error + "\n\n");
 		}
 		
-		//Otherwise create a new tracker and user and assign it to the URL
-		else {
-			var newTracker = new Tracker(newUser, data.url);
-			allTrackers[data.url] = newTracker;
-			allTrackers[data.url].numConnections = 1;
-		}
-		
-		//Get the string value for the screen resolution and add it to the payload if it doesn't exist
-		var screenResolution = newUser.getScreenResolution();
-		if(payload.screenResolutions[screenResolution]) {
-			payload.screenResolutions[screenResolution]++;
-		}
-		else {
-			payload.screenResolutions[screenResolution] = 1;
-		}
-		
-		//Add the OS to the payload if it doesn't exist
-		if(payload.os[userData.os]) {
-			payload.os[userData.os]++;
-		}
-		else {
-			payload.os[userData.os] = 1;
-		}
-		
-		//Send the data back
-		Tracker.sendPayload(allTrackers, payload, config, socket);
 	});
 	
+	//TODO can we detect disconnections from a certain page? We don't want to trigger this for dashboard disconnects
 	client.on('disconnect', function() {
-		
-		//Get the appropriate tracker to work with
-		if(allTrackers[client.url]) {
+		try {
+			//Get the appropriate tracker to work with
 			var killedTracker = allTrackers[client.url].clients[client.id];
 			
-			//TODO can we detect disconnections only from a certain socket? We don't want to trigger this for dashboard disconnects
 			//Decrement the total connections
 			payload.totalConnections--;
 			
@@ -139,10 +144,14 @@ socket.sockets.on('connection', function(client) {
 			else {
 				delete allTrackers[client.url].clients[client.id];
 			}
+			
+			//Send the data back after manipulation
+			Tracker.sendPayload(allTrackers, payload, config, socket);
+		}
+		catch(error) {
+			console.log("\n\nA disconnection error has occcured:\n" + error + "\n\n");
 		}
 		
-		//Send the data back after manipulation
-		Tracker.sendPayload(allTrackers, payload, config, socket);
 	});
 	
 });
