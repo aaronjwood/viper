9ec1999daa474c9a7d5f4ef8ade437a77a049587
Aaron Wood <aaronjwood@gmail.com>
2012-11-17 13:55:20 -0500

    Revert to sending the document's URL since the origin in the handshake object is for the actual script that is generating the request :(

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ socket.sockets.on('connection', function(client) {
 	client.on('beacon', function(data) {
 		
 		payload.totalConnections++;
+		client["url"] = data.url;
 		
 		//The client id uniquely identifies a user
 		var userData = {
@@ -68,16 +69,16 @@ socket.sockets.on('connection', function(client) {
 		var newUser = new User(userData);
 		
 		//If an object tracking the URL already exists then increment the number of connections and assign the new user
-		if(allTrackers[client.handshake.headers.origin]) {
-			allTrackers[client.handshake.headers.origin].numConnections++;
-			allTrackers[client.handshake.headers.origin].clients[client.id] = newUser;
+		if(allTrackers[data.url]) {
+			allTrackers[data.url].numConnections++;
+			allTrackers[data.url].clients[client.id] = newUser;
 		}
 		
 		//Otherwise create a new tracker and user and assign it to the URL
 		else {
-			var newTracker = new Tracker(newUser, client.handshake.headers.origin);
-			allTrackers[client.handshake.headers.origin] = newTracker;
-			allTrackers[client.handshake.headers.origin].numConnections = 1;
+			var newTracker = new Tracker(newUser, data.url);
+			allTrackers[data.url] = newTracker;
+			allTrackers[data.url].numConnections = 1;
 		}
 		
 		//Get the string value for the screen resolution and add it to the payload if it doesn't exist
@@ -106,16 +107,16 @@ socket.sockets.on('connection', function(client) {
 	client.on('disconnect', function() {
 		
 		//Ignore disconnects from the dashboard
-		if(allTrackers[client.handshake.headers.origin]) {
+		if(allTrackers[client.url]) {
 			
 			//Decrement the total connections
 			payload.totalConnections--;
 			
 			//Get the appropriate tracker to work with
-			var killedTracker = allTrackers[client.handshake.headers.origin].clients[client.id];
+			var killedTracker = allTrackers[client.url].clients[client.id];
 			
 			//Decrement the number of connections to a given URL
-			allTrackers[client.handshake.headers.origin].numConnections--;
+			allTrackers[client.url].numConnections--;
 			
 			//Decrement the appropriate browser count
 			payload.browsers.count[killedTracker.browser]--;
@@ -137,13 +138,13 @@ socket.sockets.on('connection', function(client) {
 			}
 			
 			//Remove the URL if there are no connections to it
-			if(allTrackers[client.handshake.headers.origin].numConnections == 0) {
-				delete allTrackers[client.handshake.headers.origin];
+			if(allTrackers[client.url].numConnections == 0) {
+				delete allTrackers[client.url];
 			}
 			
 			//Otherwise remove the specific client
 			else {
-				delete allTrackers[client.handshake.headers.origin].clients[client.id];
+				delete allTrackers[client.url].clients[client.id];
 			}
 			
 			//Send the data back after manipulation
