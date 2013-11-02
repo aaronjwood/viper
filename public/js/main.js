25e018ef82d5f855ea7459c1de0c609a641d63fe
Aaron Wood <aaronjwood@gmail.com>
2013-11-02 18:11:23 -0400

    Sorted the resolutions and the operating systems (offload work from the server and do this on the client side). Added some additional styling. Some formatting applied. Small optimizations applied to the client side.

--- public/js/main.js
+++ public/js/main.js
@@ -1,74 +1,102 @@
 function changeBrowserColor(browser) {
-	if(browser) {
-		return $("."+browser.toLowerCase()).css("background-color");
-	}
+    if (browser) {
+        return $("." + browser.toLowerCase()).css("background-color");
+    }
 }
 
 $(function() {
-	
-	$("#showQuickStats").on("click", function() {
-		if(parseInt($("#quickStats").css("top")) != 0) {
-			$("#quickStats").animate({"top": 0});
-		}
-		else {
-			$("#quickStats").animate({"top": -85});
-		}
-		return false;
-	});
-	
-	$(".control a").on("click", function() {
-		var control = $(this).attr("class");
-		$(".activeControl").slideUp(400, function() {
-			$("#"+control).slideDown(400).addClass("activeControl");
-		}).removeClass("activeControl");
-		return false;
-	});
-	
-	browserChart.init();
-	
-	var socket = io.connect();
-	var pages = $("#pages");
-	var totalConnections = $("#totalConnections");
-	var mostPopularBrowser = $("#mostPopularBrowser");
-	var screenResContainer = $("#resolutions");
-	var osContainer = $("#os");
-	
-	socket.on('connect', function() {
-		$("#browsers").slideDown().addClass("activeControl");
-	});
-	
-	socket.on('message', function(payload) {
-		payload = BISON.decode(payload);
-		
-		totalConnections.html(payload.totalConnections);
-		
-		var trackingData = "";
-		
-		//Get the URL and number of connections for each tracker
-		for(var i = 0; i < payload.trackers.length; i++) {
-			trackingData += "<span class='tracker'><em>" + payload.trackers[i].url + "</em> - <strong>" + payload.trackers[i].numConnections + "</strong></span><br /><br />";
-		}
-		pages.html(trackingData);
-		
-		for(var browser in payload.browsers.count) {
-			browserChart.browsers[browser] = payload.browsers.count[browser];
-		}
-		
+
+    $("#showQuickStats").on("click", function() {
+        var quickStats = $("#quickStats");
+        if (parseInt(quickStats.css("top")) != 0) {
+            quickStats.animate({"top": 0});
+        }
+        else {
+            quickStats.animate({"top": -85});
+        }
+        return false;
+    });
+
+    $(".control a").on("click", function() {
+        var control = $(this).attr("class");
+        control = $("#" + control);
+        if(control.hasClass("activeControl")) {
+            return false;
+        }
+        $(".activeControl:visible").fadeOut(300, function() {
+            control.fadeIn(300).addClass("activeControl");
+        }).removeClass("activeControl");
+        return false;
+    });
+
+    browserChart.init();
+
+    var socket = io.connect();
+    var pages = $("#pages");
+    var totalConnections = $("#totalConnections");
+    var mostPopularBrowser = $("#mostPopularBrowser");
+    var screenResContainer = $("#resolutions");
+    var osContainer = $("#os");
+
+    socket.on('connect', function() {
+        $("#browsers").fadeIn(300).addClass("activeControl");
+    });
+
+    socket.on('message', function(payload) {
+        payload = BISON.decode(payload);
+
+        totalConnections.html(payload.totalConnections);
+
+        var trackingData = "";
+
+        //Get the URL and number of connections for each tracker
+        for (var i = 0; i < payload.trackers.length; i++) {
+            trackingData += "<div><a href='"+payload.trackers[i].url+"'>" + payload.trackers[i].url + " - <strong>" + payload.trackers[i].numConnections + "</strong></div>";
+        }
+        pages.html(trackingData);
+
+        for (var browser in payload.browsers.count) {
+            browserChart.browsers[browser] = payload.browsers.count[browser];
+        }
         browserChart.repaint();
-		
+
         mostPopularBrowser.css("color", changeBrowserColor(mostPopularBrowser.html()));
-		
+
+        var resolutions = [];
         var resolutionData = "";
-        for(var resolution in payload.screenResolutions) {
-			resolutionData += "<div>" + resolution + " - " + payload.screenResolutions[resolution] + "</div>";
-    	}
+        for (var resolution in payload.screenResolutions) {
+            resolutions.push({
+                resolution: resolution,
+                count: payload.screenResolutions[resolution]
+            });
+        }
+
+        resolutions.sort(function(a, b) {
+            return b.count - a.count;
+        });
+
+        for (var i = 0; i < resolutions.length; i++) {
+            resolutionData += "<div>" + resolutions[i].resolution + " - " + resolutions[i].count + "</div>";
+        }
         screenResContainer.html(resolutionData);
-		
-		var osData = "";
-		for(var os in payload.os) {
-			osData += "<div>" + os + " - " + payload.os[os] + "</div>";
-		}
-		osContainer.html(osData);
-	});
-	
+
+        var oses = [];
+        var osData = "";
+        for (var os in payload.os) {
+            oses.push({
+                os: os,
+                count: payload.os[os]
+            });
+        }
+
+        oses.sort(function(a, b) {
+            return b.count - a.count;
+        });
+
+        for (var i = 0; i < oses.length; i++) {
+            osData += "<div>" + os + " - " + payload.os[os] + "</div>";
+        }
+        osContainer.html(osData);
+    });
+
 });
