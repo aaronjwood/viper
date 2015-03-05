cbea8aa4b0b4bd3cc635d3435a2e5eb1835fa610
Aaron Wood <aaronjwood@gmail.com>
2015-03-04 20:43:22 -0500

    iQIcBAABAgAGBQJU97S6AAoJEDAzuFK2bDAko14P/A7T2y80a8HSBg06Gy4TvAba  uYSgKapoyRcJdOEqqN+M0ggIt8k01NoJyK7wZhNyp5m6kPEh1e63OUEcVEHnK9SZ  /cg76RhJkeugOMr5gRiAQb55/gNFQvQ6+VvO7IiyHRs6McKAug4txD1YBOW307x2  YTgLxZeE9lurnnzB72RAerx43oQXZfUbv/M4gqYGdF4bv4exmfMISMb66xoh28ca  hoPh3WFa6JMYIhCQIrCvQyJpRFt5UHKaiUw7LNgp9/rz67N7x0uAwHNq8oSktqvr  hodRVJVTDFKMn7DEboLQdQDGfliW82jAiBQlCytbfvtDeC+WoRoQw7bRca8ZqdwQ  NyO34PevvHp3FqgAc1MqBH52HQNYJarUREc5OhLpbgGd3G+54ZzpNG2pV79gQ6Ti  yLru2DJEghTd/vtP91nxvGkahh4zj/6xDOPJnXxL7CJZi6oCPDCGtJcoCze4nC3l  Oa95JZxsufxUN8bGMAMsnzNj4A7KNbZCOX1dEY/OkF98UgYLjPs0UJLpGG7M0YeX  OICmmlSZE9f1iAd1jwwPAwO+pWD+yOIXchP80XARAd/1eqtpvkzQBzIvIh6/I/o7  xoPuhnSAH6ZHBaxnikNoBvrFZjuARI3QTciPTL/JGZg6XpSrdtO08fOYTTH+akZM  ES32QlmAjvA7wdPweF36  =hHLO  -----END PGP SIGNATURE-----
    Update tracking code

--- public/js/tracking.js
+++ public/js/tracking.js
@@ -1,12 +1,12 @@
 var script = document.createElement("script");
-script.src = "/socket.io/socket.io.js";
+script.src = "//localhost:3000/socket.io/socket.io.js";
 document.getElementsByTagName("head")[0].appendChild(script);
 var tracker = {
     url: document.URL,
     screenWidth: window.screen.width,
     screenHeight: window.screen.height
 };
-var socket = io.connect("/socket.io/socket.io.js");
+var socket = io.connect("//localhost:3000/socket.io/socket.io.js");
 socket.on('connect', function() {
     socket.emit("beacon", tracker);
 });
