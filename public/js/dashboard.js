4d148f9db5ae660fb2d3564e300c47822bf6a84d
Aaron Wood <aaronjwood@gmail.com>
2015-12-21 01:18:42 -0500

    Start modularizing the front end stuff a bit more

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,25 +1,3 @@
 "use strict";
 
-var dashboard = angular.module("viperDashboard", []);
-
-dashboard.service("page", function () {
-    var page;
-
-    return {
-        getPage: function () {
-            return page;
-        },
-        setPage: function (value) {
-            page = value;
-        }
-    };
-});
-
-dashboard.controller("SectionsController", function ($scope, page) {
-    $scope.page = page;
-    $scope.page.setPage("pages");
-});
-
-dashboard.controller("ContentController", function ($scope, page) {
-    $scope.page = page;
-});
+var dashboard = angular.module("dashboard", []);
