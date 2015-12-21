b156241f40a9f2ea8bb6b79bfa55e8d3431dca57
Aaron Wood <aaronjwood@gmail.com>
2015-12-21 01:12:40 -0500

    Wrap tracking code in a self exec func

--- public/js/tracking.js
+++ public/js/tracking.js
@@ -1,18 +1,20 @@
-var script = document.createElement("script");
-script.src = "//localhost:3000/socket.io/socket.io.js";
-script.async = true;
-script.onreadystatechange = script.onload = function () {
-    var state = script.readyState;
-    if (!state || /loaded|complete/.test(state)) {
-        var tracker = {
-            url: document.URL,
-            screenWidth: window.screen.width,
-            screenHeight: window.screen.height
-        };
-        var socket = io.connect("//localhost:3001/");
-        socket.on("connect", function () {
-            socket.emit("beacon", tracker);
-        });
-    }
-};
-document.getElementsByTagName("head")[0].appendChild(script);
+(function () {
+    var script = document.createElement("script");
+    script.src = "//localhost:3000/socket.io/socket.io.js";
+    script.async = true;
+    script.onreadystatechange = script.onload = function () {
+        var state = script.readyState;
+        if (!state || /loaded|complete/.test(state)) {
+            var tracker = {
+                url: document.URL,
+                screenWidth: window.screen.width,
+                screenHeight: window.screen.height
+            };
+            var socket = io.connect("//localhost:3001/");
+            socket.on("connect", function () {
+                socket.emit("beacon", tracker);
+            });
+        }
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+})();
