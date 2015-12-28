78ce959bd766df59f9b936307ba2615f59b28422
Aaron Wood <aaronjwood@gmail.com>
2015-12-28 15:31:49 -0500

    Remove the method used for sorting now that we aren't dealing with the concept of top trackers. No need for filtering and processing the trackers anymore each time we send data

--- class/Tracker.js
+++ class/Tracker.js
@@ -14,31 +14,12 @@ class Tracker {
     }
 
     /**
-     * Sort trackers by connections descending
-     * @param tracker1 First tracker used in comparison
-     * @param tracker2 Second tracker used in comparison
-     * @returns {number} Sorted trackers
-     */
-    static sortByConnections(tracker1, tracker2) {
-        return tracker2.numConnections - tracker1.numConnections;
-    }
-
-    /**
      * Sorts the trackers and sends the configured number of trackers back over the socket
      * @param tracker Main payload that the client/server uses and transfers
      * @param config Configuration settings object
      * @param socket Socket object used to push data back
      */
     static sendPayload(tracker, config, socket) {
-        var sortedTrackers = [];
-        for (var t in tracker.allTrackers) {
-            if(tracker.allTrackers.hasOwnProperty(t)) {
-                sortedTrackers.push(tracker.allTrackers[t]);
-            }
-        }
-
-        //Sort the trackers and send the top n as set in the configuration file
-        tracker.data.trackers = sortedTrackers.sort(Tracker.sortByConnections).slice(0, config.totalTrackers);
         socket.sockets.json.send(tracker.data);
     }
 }
