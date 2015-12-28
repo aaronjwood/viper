f45ab2b21dfbe10d619879fee10ef5c1097af444
Aaron Wood <aaronjwood@gmail.com>
2015-12-28 13:38:15 -0500

    Configure charts on a as-needed basis

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,3 @@
 "use strict";
 
-angular.module("dashboard", ["chart.js"]).config(function(ChartJsProvider) {
-    ChartJsProvider.setOptions({
-        responsive: true
-    });
-});
+angular.module("dashboard", ["chart.js"]);
