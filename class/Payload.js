b7f8948d969a03ca8b39269127e733e393ff57ea
Aaron Wood <aaronjwood@gmail.com>
2015-12-28 15:26:52 -0500

    Make some changes to the Payload object to reduce load on the server side. Change the structure of the browser tracking a bit. Remove trackers array and the notion of sending a certain number of trackers so that the data is more easily accessible and we don't need to loop and filter every time we send data back

--- class/Payload.js
+++ class/Payload.js
@@ -6,20 +6,18 @@ class Payload {
 Payload.allTrackers = {};
 Payload.data = {
     totalConnections: 0,
+    urls: {},
     browsers: {
-        count: {
-            "Chrome": 0,
-            "Firefox": 0,
-            "Safari": 0,
-            "Opera": 0,
-            "IE": 0,
-            "Android": 0,
-            "iPad": 0,
-            "iPhone": 0,
-            "Other": 0
-        }
+        "Chrome": 0,
+        "Firefox": 0,
+        "Safari": 0,
+        "Opera": 0,
+        "IE": 0,
+        "Android": 0,
+        "iPad": 0,
+        "iPhone": 0,
+        "Other": 0
     },
-    trackers: [],
     screenResolutions: {},
     os: {}
 };
