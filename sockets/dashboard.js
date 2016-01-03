ebe779831a5178096e9592cbd4aacb6cc246f4e4
Aaron Wood <aaronjwood@gmail.com>
2016-01-03 03:53:55 -0500

    Consistent naming

--- sockets/dashboard.js
+++ sockets/dashboard.js
@@ -1,8 +1,8 @@
 module.exports = function (socket) {
     var Payload = require("../class/Payload.js");
-    var namespacedSocket = socket.of("/dashboard");
+    var dashboardSocket = socket.of("/dashboard");
 
-    namespacedSocket.on("connection", function () {
+    dashboardSocket.on("connection", function () {
 
         //Immediately send stats to the dashboard upon request
         Payload.send(Payload, socket);
