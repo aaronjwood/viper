b1dbe792f59ab82d7b097bd0b3e41c8339109a5a
Aaron Wood <aaronjwood@gmail.com>
2017-02-09 01:00:25 -0500

    Basic display of browser data

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -23,11 +23,21 @@ module.controller("PagesController", function ($scope, socket) {
     $scope.options = {
         cutoutPercentage: 40
     };
-    $scope.labels = []
-    $scope.data = []
 
     socket.on("message", function (payload) {
         $scope.labels = Object.keys(payload.urls);
         $scope.data = Object.values(payload.urls);
     });
+});
+
+module.controller("BrowsersController", function ($scope, socket) {
+    $scope.options = {
+        cutoutPercentage: 40
+    }
+
+    socket.on("message", function (payload) {
+        $scope.labels = Object.keys(payload.browsers);
+        $scope.data = Object.values(payload.browsers);
+    });
+
 });
